6a695b94db95651b18d755951c836edcc8736edd
Benoit Paquet <benoxoft@gmail.com>
2018-06-11 18:09:50 -0400

    update comment

--- clean.py
+++ clean.py
@@ -4,9 +4,9 @@ import re
 
 df = pd.read_csv('training.1600000.processed.noemoticon.csv', encoding='cp1252', verbose="true", keep_default_na=False, na_values=[], delimiter=',', dtype=[('target', np.uint8),('ids', np.uint8), ('date', str), ('flag', str), ('user', str), ('text', str)], names=['target', 'ids', 'date', 'flag', 'user', 'text'] )
 
+
 def func(columns):
-    # @[a-z0-9_]+) : remove twitter user beginning with prefix '@'
-    # (https?://(?:[-\w.]|(?:%[\da-fA-F]{2}))+) : remove urls starting with 'http(s)'
+    # remove urls starting with 'http(s)'
     combined_re = re.compile(r"http\S+")
 
     # Lower cases
@@ -18,6 +18,7 @@ def func(columns):
     repl = r'\1'
     columns_cleaned_2ndPass = re.sub(repeat_re, repl , columns_cleaned_1rstPass )
     return(columns_cleaned_2ndPass)
+
 
 df['text'] = df['text'].apply(func)
 
